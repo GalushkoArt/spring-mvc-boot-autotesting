9741def73d2f616de1548b2de2e990bfdffb2436
Artem <Galushko@my.com>
2021-08-01 16:02:56 +0300

    Add IT test with Mocks

--- demo-application/src/main/java/com/acme/dbo/controller/AccountNotFoundException.java
+++ demo-application/src/main/java/com/acme/dbo/controller/AccountNotFoundException.java
@@ -1,6 +1,6 @@
 package com.acme.dbo.controller;
 
-public class AccountNotFoundException extends Exception {
+public class AccountNotFoundException extends RuntimeException {
     public AccountNotFoundException(Integer id) {
         super(id.toString());
     }
--- demo-application/src/test/java/com/acme/dbo/AccountCrudIT.java
+++ demo-application/src/test/java/com/acme/dbo/AccountCrudIT.java
@@ -1,8 +1,8 @@
 package com.acme.dbo;
 
-import com.acme.dbo.config.Config;
 import com.acme.dbo.config.TestConfig;
 import com.acme.dbo.controller.AccountController;
+import com.acme.dbo.controller.AccountNotFoundException;
 import com.acme.dbo.dao.AccountRepository;
 import com.acme.dbo.domain.Account;
 import org.junit.jupiter.api.Test;
@@ -13,9 +13,11 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
+import java.util.Collections;
 
 import static java.util.Arrays.asList;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -31,9 +33,40 @@ public class AccountCrudIT {
     @Test
     public void shouldGetNoAccountsWhenNoCreated() {
         when(accountRepositoryStub.findAll()).thenReturn(
-                asList(new Account(3, new BigDecimal("3.33"))));
+                Collections.emptyList());
 
-        assertTrue(accountController.findAll().contains(
-                new Account(3, new BigDecimal("3.33"))));
+        assertTrue(accountController.findAll().isEmpty());
+    }
+
+    @Test
+    public void getAllNotEmptyRepoTest() {
+        when(accountRepositoryStub.findAll()).thenReturn(
+                asList(new Account(3, new BigDecimal("3.33")), new Account(2, new BigDecimal("3.33"))));
+
+        assertTrue(accountController.findAll().containsAll(asList(
+                new Account(3, new BigDecimal("3.33")),
+                new Account(3, new BigDecimal("3.33")))));
+    }
+
+    @Test
+    public void findByIdTest() {
+        when(accountRepositoryStub.findById(5)).thenReturn(new Account(5, new BigDecimal("3.33")));
+
+        assertEquals(accountController.findById(5), new Account(5, new BigDecimal("3.33")));
+    }
+
+    @Test
+    public void findByIdNotExistAccountIdTest() {
+        when(accountRepositoryStub.findById(3)).thenReturn(null);
+
+        assertThrows(AccountNotFoundException.class, () -> accountController.findById(3));
+    }
+
+    @Test
+    public void createTest() {
+        when(accountRepositoryStub.create(new Account(1, new BigDecimal("255")))).thenReturn(new Account(1, new BigDecimal("255")));
+
+        assertEquals(accountController.create(new Account(1, new BigDecimal("255"))), new Account(1, new BigDecimal("255")));
+        verify(accountRepositoryStub).create(new Account(1, new BigDecimal("255")));
     }
 }
